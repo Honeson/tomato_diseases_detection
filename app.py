d5d63384358ef180a985c4eb3f6e878e17d40d90
Sunday Nwovu <sundaynwovuhonesty@gmail.com>
2023-05-31 14:25:16 +0100

    Added get method for homepage

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+
 import tensorflow as tf
 from PIL import Image
 from io import BytesIO
@@ -7,6 +9,20 @@ from fastapi.responses import JSONResponse
 import numpy as np
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to the tomato leaves viruses prediction!"}
+
 
 model = tf.keras.models.load_model('ml_model/model.h5')
 
@@ -17,6 +33,15 @@ def preprocess_image(image):
     image = image[tf.newaxis, ...]
 
     return image
+
+@app.get("/")
+async def predict(image: UploadFile = File(...)):
+    # Read and preprocess the image
+    img_bytes = await image.read()  # Read the image file as bytes
+    img = Image.open(BytesIO(img_bytes))  # Create a PIL image from the bytes
+    img = np.array(img)  # Convert the PIL image to a NumPy array
+    image = preprocess_image(img)
+
 
 @app.post("/predict")
 async def predict(image: UploadFile = File(...)):
